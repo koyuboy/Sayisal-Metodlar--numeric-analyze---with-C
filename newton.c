9957dae979ec1b9f5c167dc083564d8b35a7fad5
koyuboy <35155252+koyuboy@users.noreply.github.com>
2020-03-04 15:26:34 +0300

    new methods
    bicection and newton method

---
+++ newton.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <math.h>
+
+
+#define size 100
+
+double valueOfFunction(double n) {
+	return (n * n * n) - (7.0 * n * n) + (14.0 * n) - 6.0;
+}
+double valueOfDerivativeOfFunction(double n) {
+	return (3.0 * n * n) - (14.0 * n ) + 14.0;
+}
+
+int main() {
+	double a = 0.0, b = 1.0, x = 0.5;
+	double xSets[size];	//set of 
+	double f_x_Sets[size];	//set of f(x)
+
+
+	int iter = 0;
+	int j;
+	for (j = 0; ; j++) {
+		
+		double valueOfFunc = valueOfFunction(x);
+		double valueOfDerivative = valueOfDerivativeOfFunction(x);
+		
+		xSets[j] = x;
+		f_x_Sets[j] = valueOfFunc;
+
+		x = x - (valueOfFunc / valueOfDerivative);
+
+
+		iter++;
+
+		if (fabs(f_x_Sets[j]) == 0.0) {	// have a problem here.  program find root but continue to compute again and again(2 times)
+			printf("\n !! Root of Equation = %f !!\n\n", xSets[j]);
+			break;
+		}
+
+
+		if (j > 0) {	// this is perfectly work ;)
+			if (fabs(f_x_Sets[j] - f_x_Sets[j - 1]) <= 0.001) {
+				printf("\n !! Root Found  !!\n\n");
+				break;
+			}
+		}
+
+	}
+
+
+
+
+	printf("iter.	 x         f(x)\n");
+	int i;
+	for (i = 0; i < iter; i++) {
+		printf("  %d 	 %f  %f\n", i, xSets[i], f_x_Sets[i]);
+	}
+
+}
---
+++ yarilama.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <math.h>
+
+
+#define size 100
+double valueOfFunction(double n) {	
+	return (n * n * n) - (7.0 * n * n) + (14.0 * n) - 6.0;
+}
+
+
+
+int main() {
+	double a = 0.0, b = 1.0;
+	double pSets[size];	//set of p
+	double aSets[size];	//set of a
+	double bSets[size];	//set of b
+	double f_p_Sets[size];	//set of f(p)
+
+	
+	int iter=0;
+	int j;
+	for (j = 0; ; j++) {
+		aSets[j] = a;
+		bSets[j] = b;
+		pSets[j] = (a + b) / 2.0;
+		f_p_Sets[j] = valueOfFunction(pSets[j]);
+
+		if (f_p_Sets[j] > 0.0) {
+			b = pSets[j];
+		}
+		else if (f_p_Sets[j] < 0.0) {
+			a = pSets[j];
+		}
+
+		 if (fabs(f_p_Sets[j]) == 0.0) {	// have a problem here.  program find root but continue to compute again and again (many times)
+			printf("\n !! Root of Equation = %f !!\n\n", pSets[j]);
+			break;
+		}
+
+		iter++;
+
+		if (j > 0){	// this is perfectly work ;)
+			if (fabs(f_p_Sets[j] - f_p_Sets[j - 1]) <= 0.001) {
+				printf("\n !! Root Found  !!\n\n");
+				break;
+			}
+	}
+		
+	}
+
+
+
+
+	printf("iter.	 a 	  b	   p	    f(p)\n");
+	int i;
+	for (i = 0; i < iter; i++) {
+		printf("  %d 	 %f %f %f %f\n",i, aSets[i], bSets[i], pSets[i], f_p_Sets[i]);
+	}
+
+}
